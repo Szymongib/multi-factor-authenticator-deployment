4fc0eed5aacce5d4d4908e2b320605d2d5037460
Szymon Gibała <szymongib@gmail.com>
2019-06-09 12:25:39 +0200

    Mounting secrets

--- deployment/helm/installation/install.sh
+++ deployment/helm/installation/install.sh
@@ -5,4 +5,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 kubectl apply -f ${SCRIPT_DIR}/tiller.yaml
 
+# TODO - wait for tiller
+
+helm upgrade --install nginx-ingress stable/nginx-ingress --namespace default --set controller.replicaCount=1 --set rbac.create=false --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux --set controller.extraArgs.enable-ssl-passthrough=""
+
 helm upgrade --install core ${SCRIPT_DIR}/../multi-factor-authenticator
